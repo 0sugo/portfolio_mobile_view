c09a1701c1975ca0aed1bee3208585a950837f4c
Harriet Oteng <106236756+newhoteng@users.noreply.github.com>
2023-03-06 13:41:03 +0000

    Add javascript function

--- menu.js
+++ menu.js
@@ -1,1 +1,9 @@
+function toggler(){
+    var showMenu = document.querySelector(.menu-list);
+    if(showMenu.style.display === "block"){
+        showMenu.style.display = "none";
+    }else{
+        showMenu.style.display="block";
+    }
 
+}
