a58dd3b0f8d02f644520901fd2b43d4744a17041
0sugo <josugo26@gmail.com>
2023-03-07 17:33:18 +0300

    fix mobile menu window

--- menu.js
+++ menu.js
@@ -1,23 +1,21 @@
-var menu_toggle= document.getElementById("overlay");
+// var menu_toggle= document.getElementById("overlay");
 
-var cancel_toggle= document.getElementById("cancel");
+// var cancel_toggle= document.getElementById("cancel");
 
-function toggler (){
-if(menu_toggle.style.display==="block" && cancel_toggle.style.display ==="block"){
-    menu_toggle.style.display="none";
-    cancel_toggle.style.display="none";
-}else{
-    menu_toggle.style.display="block";
-    cancel_toggle.style.display="block";
-}
+// function toggler (){
+// if(menu_toggle.style.display==="block" && cancel_toggle.style.display ==="block"){
+//     menu_toggle.style.display="none";
+//     cancel_toggle.style.display="none";
+// }else{
+//     menu_toggle.style.display="block";
+//     cancel_toggle.style.display="block";
+// }
 
-}
+// }
 
-function toggler2 (){
-    if(cancel_toggle.style.display ==="block"){
-        cancel_toggle.style.display="none";
-    }else{
-        cancel_toggle.style.display="block";
-    }
-    
-    }
+const hamburger = document.querySelector(".ham");  
+const navsub = document.querySelector(".nav-sub");  
+hamburger.addEventListener('click', () => {  
+ hamburger.classList.toggle("change")  
+ navsub.classList.toggle("nav-change")  
+});  
