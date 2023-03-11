e354c66a4b5651628da8e1ccbbc8fffb170bb16f
0sugo <josugo26@gmail.com>
2023-03-11 05:27:04 +0300

    correct javascript form validation and eslint configuration

--- menu.js
+++ menu.js
@@ -212,17 +212,21 @@ window.onload = function loader() {
     });
     modal_pop.innerHTML = pops;
   }
-  const email = document.getElementById('email');
+
+
+
+  
+const email = document.getElementById('email');
 const form = document.getElementById('contact-form');
 const errorMsg = document.getElementById('form-error');
 
 form.addEventListener('submit',(event)=>{
   if (email.value !== email.value.toLowerCase()) {
-    errorMsg.style.visibility = 'visible';
+    errorMsg.style.visibility= 'visible';
     event.preventDefault();
   }else if (email.validity.typeMismatch) {
     errorMsg.textContent = 'Please enter a valid email address';
-    errorMsg.style.visibility = 'visible';
+    errorMsg.style.validity = 'visible';
     event.preventDefault();
   } else {
     errorMsg.style.visibility = 'hidden';
